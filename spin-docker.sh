86bca9666a34c6151a9b528716618bd3ae3710d8
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-04-07 04:32:27 +0530

    Added some checks in spin-docker script

--- spin-docker.sh
+++ spin-docker.sh
@@ -5,6 +5,12 @@ tag="cppdsa"
 # SCRIPT NAME
 run_script="run.sh"
 
+# Make sure docker exists
+if test command -v docker > /dev/null 2>&1; then
+    echo "Docker not found"
+    exit 1
+fi
+
 # Build the image
 docker build -t "$tag" .
 
