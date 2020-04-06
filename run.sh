ad87aefc3b02945a5f5aa3ee1bac787f2560ab42
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-04-07 04:13:11 +0530

    Added ability to pass object file name

--- run.sh
+++ run.sh
@@ -1,21 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 # ================ Check if required argument was passed or not =====================
 check_argument()
 {
-for i in "$@"
- do
-   case "$i" in
-     --no-mem) return 0 # 0 = true
-	       ;;
-   esac
-done
-return 1 # 1 = false
+    for i in "$@"
+    do
+        VALUE=$(echo $i | awk -F= '{print $2}')
+        case "$i" in
+            --no-mem) no_mem=1
+            ;;
+            -o=*) out_file=$VALUE
+            ;;
+        esac
+    done
 }
+
 # ===================================================================================
-# Output filename
+# Default output filename
 out_file="a.out"
+# Default memory check
+no_mem=0
 # ===================================================================================
+
+# Setup
+check_argument "$@"
 
 # Log compile process
 echo "[CPP RUNNER]: Compiling $1..."
@@ -29,16 +37,16 @@ if [ $? -ne 0 ]; then exit 1; fi
 # Log running process
 echo "[CPP RUNNER]: Running $1..."
 
-if check_argument "$@"; then
-  echo "[CPP RUNNER]: Opted no memory leak checks"
-  ./$out_file
-  exit 0
+if [ $no_mem -eq 1 ]; then
+    echo "[CPP RUNNER]: Opted no memory leak checks"
+    ./$out_file
+    exit 0
 fi
 
 # Check if valgrind exists
 if command -v valgrind >/dev/null 2>&1; then
-  valgrind ./$out_file
+    valgrind ./$out_file
 else
-  echo "[CPP RUNNER]: Failed to find valgrind... skipping memory checks"
-  ./$out_file
+    echo "[CPP RUNNER]: Failed to find valgrind... skipping memory checks"
+    ./$out_file
 fi
