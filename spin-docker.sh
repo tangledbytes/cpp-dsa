d7d56615a0ccf5fe3c17a01bccc29dac2f5b340d
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-04-07 04:36:46 +0530

    Added better loggin

--- spin-docker.sh
+++ spin-docker.sh
@@ -14,5 +14,8 @@ fi
 # Build the image
 docker build -t "$tag" .
 
+echo "\n=================== [CPP RUNNER] ======================== \n"
 # Run the image
 docker run "$tag" ./$run_script "$@"
+
+echo "\n[CPP RUNNER]: EXITTED WITH CODE $?"
