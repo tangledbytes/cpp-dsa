0bd9cc83b1f3c43f4bfa368331b402183f490341
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-07-21 01:02:43 +0530

    Added quick sort method

--- lib/heap.hpp
+++ lib/heap.hpp
@@ -5,303 +5,303 @@
 
 namespace utstl
 {
-class Heap
-{
-protected:
-  int *internal_arr;
-  int heap_size;
-  int max_size;
+	class Heap
+	{
+	protected:
+		int *internal_arr;
+		int heap_size;
+		int max_size;
 
-private:
-  void construct_heap(int size)
-  {
-    internal_arr = new int[size];
-    if (!internal_arr)
-    {
-      std::overflow_error("Couldn't allocate the memory!");
-    }
-    heap_size = 0;
-    max_size = size;
-  }
+	private:
+		void construct_heap(int size)
+		{
+			internal_arr = new int[size];
+			if (!internal_arr)
+			{
+				std::overflow_error("Couldn't allocate the memory!");
+			}
+			heap_size = 0;
+			max_size = size;
+		}
 
-public:
-  explicit Heap(int size) { construct_heap(size); }
+	public:
+		explicit Heap(int size) { construct_heap(size); }
 
-  int *get_heap() const { return internal_arr; }
-  int get_size() const { return heap_size; }
-  void set_size(int size)
-  {
-    if (size > -1)
-      heap_size = size;
-  }
+		int *get_heap() const { return internal_arr; }
+		int get_size() const { return heap_size; }
+		void set_size(int size)
+		{
+			if (size > -1)
+				heap_size = size;
+		}
 
-  static void max_heapify(int *arr, int size, int index);
-  static void max_heapify_iterative(int *arr, int size, int index);
-  static void min_heapify(int *arr, int size, int index);
-  static void min_heapify_iterative(int *arr, int size, int index);
+		static void max_heapify(int *arr, int size, int index);
+		static void max_heapify_iterative(int *arr, int size, int index);
+		static void min_heapify(int *arr, int size, int index);
+		static void min_heapify_iterative(int *arr, int size, int index);
 
-  ~Heap() { delete[] internal_arr; }
-};
+		~Heap() { delete[] internal_arr; }
+	};
 
-void Heap::max_heapify(int *arr, int size, int index)
-{
-  int left = (2 * index) + 1;
-  int right = (2 * index) + 2;
-  int largest = 0;
+	void Heap::max_heapify(int *arr, int size, int index)
+	{
+		int left = (2 * index) + 1;
+		int right = (2 * index) + 2;
+		int largest = 0;
 
-  if (left < size && arr[left] > arr[index])
-    largest = left;
-  else
-    largest = index;
+		if (left < size && arr[left] > arr[index])
+			largest = left;
+		else
+			largest = index;
 
-  if (right < size && arr[right] > arr[largest])
-    largest = right;
+		if (right < size && arr[right] > arr[largest])
+			largest = right;
 
-  if (largest != index)
-  {
-    utstl::utils::swap<int>(&arr[largest], &arr[index]);
-    max_heapify(arr, size, largest);
-  }
-}
+		if (largest != index)
+		{
+			utstl::utils::swap<int>(&arr[largest], &arr[index]);
+			max_heapify(arr, size, largest);
+		}
+	}
 
-void Heap::max_heapify_iterative(int *arr, int size, int index)
-{
-  int i = index;
-  int largest = 0;
-  while (true)
-  {
-    int left = (2 * i) + 1;
-    int right = (2 * i) + 2;
+	void Heap::max_heapify_iterative(int *arr, int size, int index)
+	{
+		int i = index;
+		int largest = 0;
+		while (true)
+		{
+			int left = (2 * i) + 1;
+			int right = (2 * i) + 2;
 
-    if (left < size && arr[left] > arr[i])
-      largest = left;
-    else
-      largest = i;
+			if (left < size && arr[left] > arr[i])
+				largest = left;
+			else
+				largest = i;
 
-    if (right < size && arr[right] > arr[largest])
-      largest = right;
+			if (right < size && arr[right] > arr[largest])
+				largest = right;
 
-    if (largest != i)
-    {
-      utstl::utils::swap<int>(&arr[largest], &arr[i]);
-      i = largest;
-      continue;
-    }
+			if (largest != i)
+			{
+				utstl::utils::swap<int>(&arr[largest], &arr[i]);
+				i = largest;
+				continue;
+			}
 
-    return;
-  }
-}
+			return;
+		}
+	}
 
-void Heap::min_heapify(int *arr, int size, int index)
-{
-  int left = (2 * index) + 1;
-  int right = (2 * index) + 2;
-  int smallest = 0;
+	void Heap::min_heapify(int *arr, int size, int index)
+	{
+		int left = (2 * index) + 1;
+		int right = (2 * index) + 2;
+		int smallest = 0;
 
-  if (left < size && arr[left] < arr[index])
-    smallest = left;
-  else
-    smallest = index;
+		if (left < size && arr[left] < arr[index])
+			smallest = left;
+		else
+			smallest = index;
 
-  if (right < size && arr[right] < arr[smallest])
-    smallest = right;
+		if (right < size && arr[right] < arr[smallest])
+			smallest = right;
 
-  if (smallest != index)
-  {
-    utstl::utils::swap<int>(&arr[smallest], &arr[index]);
-    min_heapify(arr, size, smallest);
-  }
-}
+		if (smallest != index)
+		{
+			utstl::utils::swap<int>(&arr[smallest], &arr[index]);
+			min_heapify(arr, size, smallest);
+		}
+	}
 
-void Heap::min_heapify_iterative(int *arr, int size, int index)
-{
-  int i = index;
-  while (true)
-  {
-    int left = (2 * i) + 1;
-    int right = (2 * i) + 2;
-    int smallest = 0;
+	void Heap::min_heapify_iterative(int *arr, int size, int index)
+	{
+		int i = index;
+		while (true)
+		{
+			int left = (2 * i) + 1;
+			int right = (2 * i) + 2;
+			int smallest = 0;
 
-    if (left < size && arr[left] < arr[i])
-      smallest = left;
-    else
-      smallest = i;
+			if (left < size && arr[left] < arr[i])
+				smallest = left;
+			else
+				smallest = i;
 
-    if (right < size && arr[right] < arr[smallest])
-      smallest = right;
+			if (right < size && arr[right] < arr[smallest])
+				smallest = right;
 
-    if (smallest != i)
-    {
-      utstl::utils::swap<int>(&arr[smallest], &arr[i]);
-      i = smallest;
-      continue;
-    }
+			if (smallest != i)
+			{
+				utstl::utils::swap<int>(&arr[smallest], &arr[i]);
+				i = smallest;
+				continue;
+			}
 
-    return;
-  }
-}
+			return;
+		}
+	}
 
-// //////////////////////////// CLASS MAXHEAP //////////////////////////////////
-// MaxHeap class encapsulates the max-heap
+	// //////////////////////////// CLASS MAXHEAP //////////////////////////////////
+	// MaxHeap class encapsulates the max-heap
 
-class MaxHeap : public Heap
-{
-private:
-  void construct_from_array(int *arr, int size)
-  {
-    heap_size = size;
-    for (int i = 0; i < size; ++i)
-      internal_arr[i] = arr[i];
-    for (int i = (size / 2) - 1; i > -1; --i)
-    {
-      max_heapify_iterative(internal_arr, heap_size, i);
-    }
-  }
+	class MaxHeap : public Heap
+	{
+	private:
+		void construct_from_array(int *arr, int size)
+		{
+			heap_size = size;
+			for (int i = 0; i < size; ++i)
+				internal_arr[i] = arr[i];
+			for (int i = (size / 2) - 1; i > -1; --i)
+			{
+				max_heapify_iterative(internal_arr, heap_size, i);
+			}
+		}
 
-public:
-  MaxHeap(int size) : Heap(size) {}
-  MaxHeap(int *arr, int size) : Heap(size) { construct_from_array(arr, size); }
+	public:
+		MaxHeap(int size) : Heap(size) {}
+		MaxHeap(int *arr, int size) : Heap(size) { construct_from_array(arr, size); }
 
-  int get_max();
-  int extract_max();
-  void increase_key(int index, int key);
-  void insert(int key);
-  int remove(int index);
-};
+		int get_max();
+		int extract_max();
+		void increase_key(int index, int key);
+		void insert(int key);
+		int remove(int index);
+	};
 
-int MaxHeap::get_max() { return internal_arr[0]; }
+	int MaxHeap::get_max() { return internal_arr[0]; }
 
-int MaxHeap::extract_max()
-{
-  if (heap_size < 1)
-    return -1;
+	int MaxHeap::extract_max()
+	{
+		if (heap_size < 1)
+			return -1;
 
-  int max = get_max();
-  internal_arr[0] = internal_arr[--heap_size];
-  max_heapify_iterative(internal_arr, heap_size, 0);
+		int max = get_max();
+		internal_arr[0] = internal_arr[--heap_size];
+		max_heapify_iterative(internal_arr, heap_size, 0);
 
-  return max;
-}
+		return max;
+	}
 
-void MaxHeap::increase_key(int index, int key)
-{
-  if (internal_arr[index] > key)
-    return;
+	void MaxHeap::increase_key(int index, int key)
+	{
+		if (internal_arr[index] > key)
+			return;
 
-  internal_arr[index] = key;
+		internal_arr[index] = key;
 
-  while (index > 0 && internal_arr[(index - 1) / 2] < internal_arr[index])
-  {
-    utils::swap<int>(&internal_arr[(index - 1) / 2], &internal_arr[index]);
+		while (index > 0 && internal_arr[(index - 1) / 2] < internal_arr[index])
+		{
+			utils::swap<int>(&internal_arr[(index - 1) / 2], &internal_arr[index]);
 
-    index = (index - 1) / 2;
-  }
-}
+			index = (index - 1) / 2;
+		}
+	}
 
-void MaxHeap::insert(int key)
-{
-  if (heap_size > max_size - 1)
-    return;
+	void MaxHeap::insert(int key)
+	{
+		if (heap_size > max_size - 1)
+			return;
 
-  internal_arr[heap_size] = INT8_MIN;
+		internal_arr[heap_size] = INT8_MIN;
 
-  increase_key(heap_size++, key);
-}
+		increase_key(heap_size++, key);
+	}
 
-int MaxHeap::remove(int index)
-{
-  if (heap_size < 1 || index > heap_size)
-    return -1;
+	int MaxHeap::remove(int index)
+	{
+		if (heap_size < 1 || index > heap_size)
+			return -1;
 
-  int value = internal_arr[index];
-  internal_arr[index] = internal_arr[--heap_size];
+		int value = internal_arr[index];
+		internal_arr[index] = internal_arr[--heap_size];
 
-  max_heapify_iterative(internal_arr, heap_size, index);
+		max_heapify_iterative(internal_arr, heap_size, index);
 
-  return value;
-}
+		return value;
+	}
 
-// //////////////////////////// CLASS MINHEAP //////////////////////////////////
-// MinHeap class encapsulates the min-heap
+	// //////////////////////////// CLASS MINHEAP //////////////////////////////////
+	// MinHeap class encapsulates the min-heap
 
-class MinHeap : public Heap
-{
-private:
-  void construct_from_array(int *arr, int size)
-  {
-    heap_size = size;
-    for (int i = 0; i < size; ++i)
-      internal_arr[i] = arr[i];
-    for (int i = (size / 2) - 1; i > -1; --i)
-    {
-      min_heapify_iterative(internal_arr, heap_size, i);
-    }
-  }
+	class MinHeap : public Heap
+	{
+	private:
+		void construct_from_array(int *arr, int size)
+		{
+			heap_size = size;
+			for (int i = 0; i < size; ++i)
+				internal_arr[i] = arr[i];
+			for (int i = (size / 2) - 1; i > -1; --i)
+			{
+				min_heapify_iterative(internal_arr, heap_size, i);
+			}
+		}
 
-public:
-  MinHeap(int size) : Heap(size) {}
-  MinHeap(int *arr, int size) : Heap(size) { construct_from_array(arr, size); }
-  MinHeap(int *arr, int size, int max_size) : Heap(max_size)
-  {
-    construct_from_array(arr, size);
-  }
+	public:
+		MinHeap(int size) : Heap(size) {}
+		MinHeap(int *arr, int size) : Heap(size) { construct_from_array(arr, size); }
+		MinHeap(int *arr, int size, int max_size) : Heap(max_size)
+		{
+			construct_from_array(arr, size);
+		}
 
-  int get_min();
-  int extract_min();
-  void decrease_key(int index, int key);
-  void insert(int key);
-  int remove(int index);
-};
+		int get_min();
+		int extract_min();
+		void decrease_key(int index, int key);
+		void insert(int key);
+		int remove(int index);
+	};
 
-int MinHeap::get_min() { return internal_arr[0]; }
-int MinHeap::extract_min()
-{
-  if (heap_size < 1)
-    return -1;
+	int MinHeap::get_min() { return internal_arr[0]; }
+	int MinHeap::extract_min()
+	{
+		if (heap_size < 1)
+			return -1;
 
-  int min = get_min();
+		int min = get_min();
 
-  internal_arr[0] = internal_arr[--heap_size];
-  min_heapify_iterative(internal_arr, heap_size, 0);
+		internal_arr[0] = internal_arr[--heap_size];
+		min_heapify_iterative(internal_arr, heap_size, 0);
 
-  return min;
-}
+		return min;
+	}
 
-void MinHeap::decrease_key(int index, int key)
-{
-  if (internal_arr[index] < key)
-    return;
+	void MinHeap::decrease_key(int index, int key)
+	{
+		if (internal_arr[index] < key)
+			return;
 
-  internal_arr[index] = key;
+		internal_arr[index] = key;
 
-  while (index > 0 && internal_arr[(index - 1) / 2] > internal_arr[index])
-  {
-    utils::swap<int>(&internal_arr[(index - 1) / 2], &internal_arr[index]);
-    index = (index - 1) / 2;
-  }
-}
+		while (index > 0 && internal_arr[(index - 1) / 2] > internal_arr[index])
+		{
+			utils::swap<int>(&internal_arr[(index - 1) / 2], &internal_arr[index]);
+			index = (index - 1) / 2;
+		}
+	}
 
-void MinHeap::insert(int key)
-{
-  if (heap_size > max_size - 1)
-    return;
+	void MinHeap::insert(int key)
+	{
+		if (heap_size > max_size - 1)
+			return;
 
-  internal_arr[heap_size] = INT8_MAX;
+		internal_arr[heap_size] = INT8_MAX;
 
-  decrease_key(heap_size++, key);
-}
+		decrease_key(heap_size++, key);
+	}
 
-int MinHeap::remove(int index)
-{
-  if (heap_size < 1 || index > heap_size)
-    return -1;
+	int MinHeap::remove(int index)
+	{
+		if (heap_size < 1 || index > heap_size)
+			return -1;
 
-  int value = internal_arr[index];
-  internal_arr[index] = internal_arr[--heap_size];
+		int value = internal_arr[index];
+		internal_arr[index] = internal_arr[--heap_size];
 
-  min_heapify_iterative(internal_arr, heap_size, index);
+		min_heapify_iterative(internal_arr, heap_size, index);
 
-  return value;
-}
+		return value;
+	}
 
 } // namespace utstl
--- lib/sort.hpp
+++ lib/sort.hpp
@@ -2,92 +2,138 @@
 #include "heap.hpp"
 #include "utils/utils.hpp"
 
-namespace {
-int *merge_into_one(int *arr1, int *arr2, int size1, int size2) {
-  int *res = new int[size1 + size2];
-  int index1 = 0;
-  int index2 = 0;
-  int i = 0;
+namespace
+{
+	int *merge_into_one(int *arr1, int *arr2, int size1, int size2)
+	{
+		int *res = new int[size1 + size2];
+		int index1 = 0;
+		int index2 = 0;
+		int i = 0;
 
-  while (index1 < size1 && index2 < size2) {
-    if (arr1[index1] <= arr2[index2]) {
-      res[i++] = arr1[index1++];
-    } else {
-      res[i++] = arr2[index2++];
-    }
-  }
+		while (index1 < size1 && index2 < size2)
+		{
+			if (arr1[index1] <= arr2[index2])
+			{
+				res[i++] = arr1[index1++];
+			}
+			else
+			{
+				res[i++] = arr2[index2++];
+			}
+		}
 
-  while (index1 < size1) {
-    res[i++] = arr1[index1++];
-  }
+		while (index1 < size1)
+		{
+			res[i++] = arr1[index1++];
+		}
 
-  while (index2 < size2) {
-    res[i++] = arr2[index2++];
-  }
+		while (index2 < size2)
+		{
+			res[i++] = arr2[index2++];
+		}
 
-  return res;
-}
-}  // namespace
+		return res;
+	}
 
-namespace utstl {
+	int partition(int *arr, int l, int r)
+	{
+		int pivot = arr[l];
+		int j = l;
 
-namespace Sort {
+		for (int i = l + 1; i <= r; ++i)
+		{
+			if (arr[i] <= pivot)
+			{
+				utstl::utils::swap<int>(&arr[i], &arr[++j]);
+			}
+		}
 
-// Performs merge sort on the given array
-// Returns sorted array
-int *merge(int arr[], int start, int end) {
-  if (start == end) {
-    int *res = new int[1];
-    res[0] = arr[start];
-    return res;
-  }
+		utstl::utils::swap<int>(&arr[j], &arr[l]);
 
-  int mid = (start + end) / 2;
-  int *arr1 = merge(arr, start, mid);
-  int *arr2 = merge(arr, mid + 1, end);
+		return j;
+	}
+} // namespace
 
-  int *res = merge_into_one(arr1, arr2, mid - start + 1, end - (mid + 1) + 1);
+namespace utstl
+{
 
-  // Delete the arrays
-  delete[] arr1;
-  delete[] arr2;
+	namespace Sort
+	{
 
-  return res;
-}
+		// Performs merge sort on the given array
+		// Returns sorted array
+		int *merge(int arr[], int start, int end)
+		{
+			if (start == end)
+			{
+				int *res = new int[1];
+				res[0] = arr[start];
+				return res;
+			}
 
-// Performs insertion sort on the given array
-void insertion(int arr[], int size) {
-  for (int i = 0; i < size - 1; ++i) {
-    int j = i;
-    int key = i + 1;
-    while (j > -1 && arr[j] > arr[key]) {
-      utstl::utils::swap<int>(&arr[j--], &arr[key--]);
-    }
-  }
-}
+			int mid = (start + end) / 2;
+			int *arr1 = merge(arr, start, mid);
+			int *arr2 = merge(arr, mid + 1, end);
 
-// Performs heap sort on the given array
-int *heap(int arr[], int size) {
-  MaxHeap heap(arr, size);
-  int *heapArr = new int[size];
-  int *tempArr = heap.get_heap();
+			int *res = merge_into_one(arr1, arr2, mid - start + 1, end - (mid + 1) + 1);
 
-  // Store the heap into the array
-  for (int i = 0; i < size; ++i) heapArr[i] = tempArr[i];
+			// Delete the arrays
+			delete[] arr1;
+			delete[] arr2;
 
-  for (int i = size - 1; i > 0; --i) {
-    // Exchange first and ith element
-    utils::swap<int>(&heapArr[0], &heapArr[i]);
+			return res;
+		}
 
-    // Decrement heap size
-    size--;
+		// Performs insertion sort on the given array
+		void insertion(int arr[], int size)
+		{
+			for (int i = 0; i < size - 1; ++i)
+			{
+				int j = i;
+				int key = i + 1;
+				while (j > -1 && arr[j] > arr[key])
+				{
+					utstl::utils::swap<int>(&arr[j--], &arr[key--]);
+				}
+			}
+		}
 
-    // Heapify the remaining array
-    Heap::max_heapify(heapArr, size, 0);
-  }
+		// Performs heap sort on the given array
+		int *heap(int arr[], int size)
+		{
+			MaxHeap heap(arr, size);
+			int *heapArr = new int[size];
+			int *tempArr = heap.get_heap();
 
-  return heapArr;
-}
+			// Store the heap into the array
+			for (int i = 0; i < size; ++i)
+				heapArr[i] = tempArr[i];
 
-}  // namespace Sort
-}  // namespace utstl
+			for (int i = size - 1; i > 0; --i)
+			{
+				// Exchange first and ith element
+				utils::swap<int>(&heapArr[0], &heapArr[i]);
+
+				// Decrement heap size
+				size--;
+
+				// Heapify the remaining array
+				Heap::max_heapify(heapArr, size, 0);
+			}
+
+			return heapArr;
+		}
+
+		void quick(int *arr, int l, int r)
+		{
+			if (l < r)
+			{
+				int pivot = partition(arr, l, r);
+				quick(arr, l, pivot - 1);
+				quick(arr, pivot + 1, r);
+			}
+		}
+
+	} // namespace Sort
+} // namespace utstl
